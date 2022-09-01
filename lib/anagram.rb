f3c43842874d474402ed979ecc8632b268a27a32
Gerren <g.ballard23@outlook.com>
2022-09-01 17:12:44 -0500

    commit tree 8fc6f32a55956186383fc51395d2f6024a4dc8fd

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram 
+
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match arr
+      arr.filter do |i|
+        if @word.chars.sort == i.chars.sort 
+           i
+        end
+      end      
+    end
+end
